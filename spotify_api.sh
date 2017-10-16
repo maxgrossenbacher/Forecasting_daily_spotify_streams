3d97bdbea606bce9d6cde9c5daeadff62b67e045
Max Grossenbacher <grossenbacher.max@gmail.com>
2017-10-16 11:46:18 -0600

    Update spotify_api.sh

--- spotify_api.sh
+++ spotify_api.sh
@@ -4,4 +4,4 @@ wc $url
 
 curl -X GET $url \
 -H "Accept: application/json" \
--H "Accept: application/json" -H "Authorization: Bearer BQAV3sZQ8woY4Fa2nPGLZZmLNPHz7tqWUtt4RJvzp6i2Wb-0QjoI68MXrxuNdauH_4coBLIxjQcvu_9xJ23J-5d4BY1bRAM_GVwflGrmb0xo9bzeVICB57udeejB1ugfxaPZVws7-0kriQ"
+-H "Accept: application/json" -H "Authorization: Bearer ${Otoken}"
